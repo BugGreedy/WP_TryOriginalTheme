6373d51553c48f4b6aa9a6824872acbc302a8a39
BugGreedy <desire.cutton.bug.1130@gmail.com>
2021-04-28 13:53:21 +0900

    CSS前まで

--- index.php
+++ index.php
@@ -71,17 +71,18 @@
   <div class="container">
     <div class="row">
       <div class="col-lg-8 col-md-10 mx-auto">
+        <?php the_post(); ?>
         <div class="post-preview">
           <a href="post.html">
             <h2 class="post-title">
               <?php the_title(); ?>
             </h2>
             <h3 class="post-subtitle">
-              Problems look mighty small from 150 miles up
+              <?php the_content(); ?>
             </h3>
           </a>
           <p class="post-meta">Posted by
-            <a href="#">Start Bootstrap</a>
+            <?php the_author(); ?>
             on <?php the_time('Y/m/d/g:ia'); ?></p>
         </div>
         <hr>
